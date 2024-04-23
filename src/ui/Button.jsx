e2fa0f09a1aa498e73d90304a6819f92fe762e57
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-04-23 08:10:05 +0200

    Add the structure and styles using tailwind

---
+++ src/ui/Button.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+
+function Button({ children, disabled, to, type }) {
+  const base =
+    'inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors text-sm duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
+
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-3 py-2 md:px-5 md:py-2 text-xs',
+    secondary:
+      'inline-block text-sm rounded-full border-stone-300 px-3 py-2 md:px-5 md:py-3 border-2 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none hover:text-stone-800 focus:text-stone-800 focus:ring  focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed ',
+  };
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
