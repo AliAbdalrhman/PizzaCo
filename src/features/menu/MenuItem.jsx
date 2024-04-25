ba803f2c84a2e1ce40d0d167ea4326d0488f892f
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-04-25 13:01:01 +0200

    Add and delete items from cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,9 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import DeleteItem from '../cart/DeleteItem';
+
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch()
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+
+  const isInCart = currentQuantity > 0;
+  
+  function handleAddToCart(){
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1
+    }
+
+    dispatch(addItem(newItem))
+  }
+  
   return (
     <li className="flex gap-4 py-2">
       <img
@@ -25,7 +47,10 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          
+         {isInCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !isInCart && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
         </div>
       </div>
     </li>
@@ -33,3 +58,4 @@ function MenuItem({ pizza }) {
 }
 
 export default MenuItem;
+
