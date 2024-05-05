28c37814fd17a8b155e38f035dae151cfbbf9021
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-05-05 15:51:16 +0300

    Use geolocation to find my location automatically and display it in address

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,10 +2,11 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { useState } from 'react';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../../store'
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -42,7 +43,16 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
-  const username = useSelector((state) => state.user.username);
+  const {
+  username,
+  status:addressStatus,
+  position,
+  address,
+  error: errorAddress
+} = useSelector((state) => state.user);
+const isLoadingAddress = addressStatus === 'loading';
+
+  const dispatch = useDispatch();
 
   const formErrors = useActionData();
 
@@ -81,16 +91,35 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center relative">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
               className="input w-full"
             />
+            {addressStatus === 'error' && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && 
+          <span className='absolute right-[5px] top-[44px] sm:top-[4px] sm:right-[8px] md:top-[8px] md:right-[8px] z-50'>
+            <Button 
+              disabled={isLoadingAddress}
+              type="small" 
+              onClick={(e) => {
+                e.preventDefault()
+                dispatch(fetchAddress())
+            }
+              }>Get Position</Button>
+          </span>}
+          
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -109,7 +138,12 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
+          <input 
+            type='hidden' 
+            name="position" 
+            value={position.latitude && position.longitude ? `${position.latitude},${position.longitude}` : ""} 
+          />
+          <Button disabled={isSubmitting || isLoadingAddress} type="primary">
             {isSubmitting ? 'Placing your order...' : `Order now from ${totalPrice}$`}
           </Button>
         </div>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,13 @@
-/*function getPosition() {
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getAddress } from '../../services/apiGeocoding'
+
+function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 }
 
-async function fetchAddress() {
+export const fetchAddress = createAsyncThunk('user/fetchAddress', async function() {
   // 1) We get the user's geolocation position
   const positionObj = await getPosition();
   const position = {
@@ -17,13 +20,13 @@ async function fetchAddress() {
   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
   // 3) Then we return an object with the data that we are interested in
-  return { position, address };
-} */
-
-import { createSlice } from '@reduxjs/toolkit';
-
+  return { position, address };})
 const initialState = {
   username: '',
+  status: 'idle',
+  position: {},
+  address: '',
+  error: ''
 };
 
 const userSlice = createSlice({
@@ -34,6 +37,17 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder)=>builder
+  .addCase(fetchAddress.pending,(state)=>{state.status = 'loading'})
+  .addCase(fetchAddress.fulfilled,(state,action) => {
+    state.position = action.payload.position;
+    state.address = action.payload.address;
+    state.status = 'idle'
+  })
+  .addCase(fetchAddress.rejected,(state,action) => {
+    state.status = 'error';
+    state.error = action.error.message;
+  })
 });
 
 export const { updateName } = userSlice.actions;
